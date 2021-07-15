ffd7c8bac014f8ffac7c3097bfb02fb61d0a6001
Laerthe Souza <laerte_souza.developer@outlook.com>
2021-07-15 00:27:13 -0300

    Finished challenge

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
+import { Dashboard } from '../pages/Dashboard';
 
-const Routes = () => (
+export const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
   </Switch>
 );
-
-export default Routes;
